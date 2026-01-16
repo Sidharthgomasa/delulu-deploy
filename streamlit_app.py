07ae720c4161dfc925abc78e33cff948f681ecb5
Sidharth Gomasa <sidharthgomasa04@gmail.com>
2026-01-16 10:43:08 +0530

    Add streamlit entry file

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Delulu Meter", page_icon="😵‍💫")
+
+st.title("😵‍💫 Delulu Meter")
+st.subheader("How delulu are you today?")
+
+st.write("If you see this, deployment is successful 🚀🔥")
